7afc5980344bd586b7cdb1692559f5b6f41fa9c8
Aklile Yilma <aklileyilma@gmail.com>
2023-03-28 13:02:29 +0300

    testing for functions tolower, toupper and capitalize

--- string_functions.py
+++ string_functions.py
@@ -1,30 +1,40 @@
 def to_upper(string):
     result = []
 
-    for char in string:
-        if 97 <= ord(char) <= 122:
-            result.append(chr(ord(char) - 32))
-        else:
-            result.append(char)
+    if not isinstance(string, str):
+        raise TypeError("input should be of type string")
+    else:
+        for char in string:
+            if 97 <= ord(char) <= 122:
+                result.append(chr(ord(char) - 32))
+            else:
+                result.append(char)
 
     return "".join(result)
 
 def to_lower(string):
     result = []
-
-    for char in string:
-        if 65 <= ord(char) <= 90:
-            result.append(chr(ord(char) - 32))
-        else:
-            result.append(char)
-            
+    
+    if not isinstance(string, str):
+        raise TypeError("input should be of type string")
+    else:
+        for char in string:
+            if 65 <= ord(char) <= 90:
+                result.append(chr(ord(char) - 32))
+            else:
+                result.append(char)
+                
     return "".join(result)
 
 def capitalize(string):
     result = ""
 
-    if result:
-        result += to_upper(string[0])
-    result += to_lower(string[1:])
+    if not isinstance(string, str):
+        raise TypeError("input should be of type string")
+    else:
+        if result:
+            result += to_upper(string[0])
+        result += to_lower(string[1:])
 
+    print("here",result)
     return result
--- test.py
+++ test.py
@@ -3,15 +3,38 @@ from string_functions import *
 
 class TestToUpper(unittest.TestCase):
     # write your code here
-    pass
-
+    def testToUpper(self):
+        test_string = "cat"
+        self.assertEqual(test_string.upper(), to_upper(test_string))
+        
+        test_string = 1
+        with self.assertRaises(TypeError):
+            to_upper(test_string)
+            
+        # self.assertRaises(TypeError, to_upper(test_string))
+    
 class TestToLower(unittest.TestCase):
     # Write you code here
-    pass
-
+    def testToLower(self):
+        test_string = "cat"
+        self.assertEqual(test_string.lower(), to_lower(test_string))
+        
+        test_string = 1
+        with self.assertRaises(TypeError):
+            to_lower(test_string)
+        # self.assertRaises(TypeError, to_lower(test_string))
+    
 class TestCapitalize(unittest.TestCase):
     # Write your code here
-    pass
+    def testCapitalize(self):
+        test_string = "cat"
+        self.assertEqual(test_string.capitalize(), capitalize(test_string))
+        
+        test_string = 1
+        with self.assertRaises(TypeError):
+            to_lower(test_string)
+        # self.assertRaises(TypeError, to_lower(test_string))
+    
 
 if __name__ == '__main__':
     unittest.main()
